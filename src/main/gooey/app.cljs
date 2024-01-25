5261dcb9d1ff684281aee359e39119a2c3fcf2c4
David Roussov <david.roussov@gmail.com>
2024-01-25 22:39:49 +1100

    add page structure

--- src/main/gooey/app.cljs
+++ src/main/gooey/app.cljs
@@ -2,12 +2,57 @@
   (:require [reagent.core :as reagent]
             [cljsjs.react]))
 
-(defn hello-world []
-  [:div
-   [:h1 "Hello, World!"]])
+(defn app []
+  [:div {:class "container my-5"}
+    [:div {:class "row"}
+      [:div {:class "col-12"}
+        [:h1 {:class "display-1 fw-bolder"} "Gooey"]
+      ]
+    ]
+    [:div {:class "row"}
+      [:div {:class "col-12"}
+        [:div {:class "mt-5"}
+          [:h2 {:id "document" :class "display-4 fw-bolder"} 
+            "Document"
+          ]
+          [:textarea {:class "form-control" :rows "20"}
+
+          ]
+        ]
+      ]
+    ]
+    [:div {:class "row"}
+      [:div {:class "col-12"}
+        [:div {:class "mt-5"}
+          [:h2 {:id "document" :class "display-4 fw-bolder"} 
+            "Parser"
+          ]
+        ]
+      ]
+    ]
+    [:div {:class "row"}
+      [:div {:class "col-12"}
+        [:div {:class "mt-5"}
+          [:h2 {:id "document" :class "display-4 fw-bolder"} 
+            "Output"
+          ]
+        ]
+      ]
+    ]
+    [:div {:class "row"}
+      [:div {:class "col-12"}
+        [:div {:class "mt-5"}
+          [:h2 {:id "document" :class "display-4 fw-bolder"} 
+            "Interface"
+          ]
+        ]
+      ]
+    ]
+  ]
+)
 
 (defn mount-root []
-  (reagent/render [hello-world]
+  (reagent/render [app]
                   (.getElementById js/document "root")))
 
 (defn init []
