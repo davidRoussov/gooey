a42207f0291817d7aaea97148cff410bf868c317
David Roussov <david.roussov@gmail.com>
2024-01-28 11:02:53 +1100

    add reitit routing

--- src/main/gooey/app.cljs
+++ src/main/gooey/app.cljs
@@ -1,9 +1,14 @@
 (ns gooey.app
-  (:require [reagent.core :as reagent]
-            [cljsjs.react]))
+  (:require [reagent.core :as r]
+            [reitit.frontend :as rf]
+            [reitit.frontend.easy :as rfe]
+            [reitit.coercion.spec :as rss]
+            [spec-tools.data-spec :as ds]
+  )
+)
 
-(defn app []
-  (let [parser-output (reagent/atom "")]
+(defn home-page []
+  (let [parser-output (r/atom "")]
     (fn []
       [:div {:class "container my-5"}
         [:div {:class "row"}
@@ -43,7 +48,6 @@
                   :on-change #(reset! parser-output (-> % .-target .-value))
                 }
               ]
-              [:p "u entered: " @parser-output]
             ]
           ]
         ]
@@ -61,9 +65,34 @@
   )
 )
 
-(defn mount-root []
-  (reagent/render [app]
-                  (.getElementById js/document "root")))
+(defn chat-page []
+  [:div
+    [:h2 "Chat"]
+  ]
+)
 
-(defn init []
-  (mount-root))
+(defonce match (r/atom nil))
+
+(defn app []
+  [:div
+    (if @match
+      (let [view (:view (:data @match))]
+        [view @match]))
+  ]
+)
+
+(def routes
+  [
+   ["/" {:name ::home :view home-page}]
+   ["/chat" {:name ::chat :view chat-page}]
+  ]
+)
+
+(defn init! []
+  (rfe/start!
+    (rf/router routes {:data {:coercion rss/coercion}})
+    (fn [m] (reset! match m))
+    {:use-fragment false})
+  (r/render [app] (.getElementById js/document "root")))
+
+(init!)
